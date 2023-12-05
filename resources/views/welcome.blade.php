1c79744e2adc7cd8610d98f76d13a156225c6a5a
kskhan1911 <68466116+kskhan1911@users.noreply.github.com>
2023-12-05 12:01:21 +0600

    Welcome Page Mobile Responsive Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         <div class="relative flex justify-center min-h-screen py-4 bg-gray-100 items-top gray:bg-gray-900 sm:items-center sm:pt-0" style="background-image: url('https://dupulse.com/wp-content/uploads/2023/11/welcomebackground-1024x576.png');
         height: 100%; background-size:cover; ">
             @if (Route::has('login'))
-                <div class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
+                <div class="fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline dark:text-gray-500">Dashboard</a>
                     @else
